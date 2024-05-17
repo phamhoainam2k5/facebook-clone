187db9edfab865e8514d2312fff813e67801947f
hoainam <phahoainam2005@gmail.com>
2024-05-17 15:00:27 +0700

    friends

--- src/page/RightSide/RightSide.jsx
+++ src/page/RightSide/RightSide.jsx
@@ -7,11 +7,38 @@ function RightSide() {
     <div className='right-container'>
         <div className='right-header'>
             <h3>Contacts</h3>
-            <AvataLayout name={"Phạm Văn Tuấn"} />
-            <AvataLayout name={"Nguyễn Thị Thu Hằng"} />
-            <AvataLayout name={"Phạm Thị Thanh Nhàn"} />
-            <AvataLayout name={"Nguyễn Thị Hoàng Thủy"} />
-            
+            <AvataLayout name={"An Nguyễn"} />
+            <AvataLayout name={"Hằng"} />
+            <AvataLayout name={"Thanh Nhàn"} />
+            <AvataLayout name={"Lò Thị Nga"} />
+            <AvataLayout name={"Lê Thị Cẩm Hiếu"} />
+            <AvataLayout name={"Ngọc Thúy"} />
+            <AvataLayout name={"Oanh Lê"} />
+            <AvataLayout name={"Nguyễn Trọng Duy"} />
+            <AvataLayout name={"Tuấn Béo"} />
+            <AvataLayout name={"Trang Híp"} />
+            <AvataLayout name={"Hiếu Đây"} />
+            <AvataLayout name={"Tiên"} />
+            <AvataLayout name={"Nhân T.Nguyễn"} />
+            <AvataLayout name={"Thủy"} />
+            <AvataLayout name={"Không có tên"} />
+            <AvataLayout name={"Mạnh Đoàn"} />
+            <AvataLayout name={"Giang Con"} />
+            <AvataLayout name={"JOHN Doe"} />
+            <AvataLayout name={"Adam"} />
+            <AvataLayout name={"Senna"} />
+            <AvataLayout name={"Lautinus"} />
+            <AvataLayout name={"Kalista"} />
+            <AvataLayout name={"Yasuo"} />
+            <AvataLayout name={"Lee"} />
+            <AvataLayout name={"Ashe"} />
+            <AvataLayout name={"Lulu"} />
+            <AvataLayout name={"Wibu"} />
+            <AvataLayout name={"King"} />
+            <AvataLayout name={"Queen"} />
+            <AvataLayout name={"Canh Cổng"} />
+            <AvataLayout name={"Bảo vệ"} />
+            <AvataLayout name={"Shipper"} />
         </div>
     </div>
   )
